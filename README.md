000c1f740ab263573d0532d846bcf7d861a51a26
Henrique Thorp Küsel <hoofos@gmail.com>
2019-04-02 09:48:33 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,28 @@
+Udacity - Nanodegree Web Front-End Avançado
+
+RESTAURANT REVIEWS - STAGE ONE
+
+1.O PROJETO
+=> A partir de um site estático fornecido, foram efetuadas alterações de código e de layout, deixando-o mais accessível e responsivo. Também foi adicionado o ServiceWorker, permitindo a navegação offline a partir do cache.
+
+2.ARQUIVOS
+
+css\style.css
+data\restaurants.json
+img\1.jpg
+img\2.jpg
+img\3.jpg
+img\4.jpg
+img\5.jpg
+img\6.jpg
+img\7.jpg
+img\8.jpg
+img\9.jpg
+img\10.jpg
+js\dbhelper.js
+js\main.js
+js\restaurant_info.js
+index.html
+README.ms
+restaurant.html
+sw.js
---
+++ sw.js
@@ -0,0 +1,79 @@
+let staticCacheName = 'restaurant-review-cache';
+
+/**
+ * What to cache.
+ */
+let urlsToCache = [
+  '/index.html',
+  '/restaurant.html',
+  '/css/styles.css',
+  '/js/dbhelper.js',
+  '/js/main.js',
+  '/js/restaurant_info.js',
+  '/data/restaurants.json',
+  '/img/1.jpg',
+  '/img/2.jpg',
+  '/img/3.jpg',
+  '/img/4.jpg',
+  '/img/5.jpg',
+  '/img/6.jpg',
+  '/img/7.jpg',
+  '/img/8.jpg',
+  '/img/9.jpg',
+  '/img/10.jpg'
+];
+
+/**
+ * Event Listener - Install
+ */
+self.addEventListener('install', function(event) {
+  console.log('ServiceWorker Installed');
+
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      console.log('ServiceWorker Caching');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+/**
+ * Event Listener - Activate
+ */
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('restaurant-') && cacheName !== staticCacheName;
+        }).map(function(cacheName) {
+          return cache.delete(cacheName);
+        })
+      ).then(function() {
+        console.log('ServiceWorker Activated');
+      });
+    })
+  );
+});
+
+/**
+ * Event Listener - Fetch
+ */
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    }).catch(function(err) {
+      console.log('Error: ' + err);
+    })
+  );
+});
+
+/**
+ * Event Listener - Message
+ */
+self.addEventListener('message', function(event) {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
